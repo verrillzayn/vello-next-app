9863f637e3106df465fb1a22cdd486bf185e950d
verzynx <verzynx@gmail.com>
2024-01-20 03:39:54 +0700

    chore: adding format currency utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    locale?: "en-US" | "fr-FR" | "en-GB" | "bn-BD" | "id-ID";
+    currency?: "USD" | "EUR" | "GBP" | "BDT" | "IDR";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {},
+) {
+  const { currency = "IDR", notation = "standard", locale = "id-ID" } = options;
+  // const { currency = "USD", notation = "standard", locale = "en-US" } = options;
+  // const { currency = "EUR", notation = "standard", locale = "en-GB" } = options;
+
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency,
+    notation,
+  }).format(numericPrice);
 }
