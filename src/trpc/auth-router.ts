1eea2aef3585e439f0568bed377d1562e31424e0
verzynx <verzynx@gmail.com>
2024-01-21 00:50:45 +0700

    chore: create auth api

---
+++ src/trpc/auth-router.ts
@@ -0,0 +1,33 @@
+import { AuthCredentialsValidator } from "../lib/validators/account-credential-validator";
+import { publicProcedure, router } from "./trpc";
+import { getPayloadClient } from "../get-payload";
+import { TRPCError } from "@trpc/server";
+
+export const authRouter = router({
+  createPayloadUser: publicProcedure
+    .input(AuthCredentialsValidator)
+    .mutation(async ({ input }) => {
+      const { email, password } = input;
+
+      const payload = await getPayloadClient();
+
+      //chek if user already exist
+      const { docs: users } = await payload.find({
+        collection: "user",
+        where: {
+          email: {
+            equals: email,
+          },
+        },
+      });
+
+      if (users.length !== 0) {
+        throw new TRPCError({ code: "CONFLICT" });
+      }
+
+      await payload.create({
+        collection: "user",
+        data: {},
+      });
+    }),
+});
