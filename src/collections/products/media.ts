c048afe7e3810688c7382b42d153f96c7fcdf4d8
verzynx <verzynx@gmail.com>
2024-01-30 19:27:06 +0700

    feat: adding collections

---
+++ src/collections/products/media.ts
@@ -0,0 +1,81 @@
+import { User } from "../../payload-types";
+import { Access, CollectionConfig } from "payload/types";
+
+const isAdminOrAccessToImages =
+  (): Access =>
+  async ({ req }) => {
+    const user = req.user as User | undefined;
+
+    if (!user) return false;
+    if (user.role === "admin") return true;
+
+    return {
+      user: {
+        equals: req.user.id,
+      },
+    };
+  };
+
+export const Media: CollectionConfig = {
+  slug: "media",
+  hooks: {
+    beforeChange: [
+      ({ req, data }) => {
+        return { ...data, user: req.user.id };
+      },
+    ],
+  },
+  admin: {
+    hidden: ({ user }) => user.role !== "admin",
+  },
+  access: {
+    read: async ({ req }) => {
+      const referer = req.headers.referer;
+
+      if (!referer?.includes("sell") || !req.user) {
+        return true;
+      }
+
+      return await isAdminOrAccessToImages()({ req });
+    },
+    delete: isAdminOrAccessToImages(), //this is secara teknikal passing req sama kaya  ({req}) => isAdminOrAccessToImages()({req})
+    update: isAdminOrAccessToImages(),
+  },
+  upload: {
+    staticURL: "/media",
+    staticDir: "media",
+    imageSizes: [
+      {
+        name: "thumbnail",
+        width: 400,
+        height: 300,
+        position: "centre",
+      },
+      {
+        name: "tablet",
+        width: 1024,
+        height: undefined,
+        position: "centre",
+      },
+      {
+        name: "card",
+        width: 768,
+        height: 1024,
+        position: "centre",
+      },
+    ],
+    mimeTypes: ["image/*"],
+  },
+  fields: [
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
+    },
+  ],
+};
