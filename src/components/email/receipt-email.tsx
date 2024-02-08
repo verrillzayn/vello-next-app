96aa4122bc4fe647981141fdd359fe1f26e22472
verzynx <verzynx@gmail.com>
2024-02-08 11:27:27 +0700

    feat: create email components (receipt and verify)

--- src/components/email/receipt-email.tsx
+++ src/components/email/receipt-email.tsx
@@ -43,10 +43,10 @@ const ReceiptEmail = ({
           <Section>
             <Column>
               <Img
-                src={`${process.env.NEXT_PUBLIC_SERVER_URL}/hippo-email-sent.png`}
+                src={`${process.env.NEXT_PUBLIC_SERVER_URL}/vello-email-sent.png`}
                 width="100"
                 height="100"
-                alt="DigitalHippo"
+                alt="Vello"
               />
             </Column>
 
